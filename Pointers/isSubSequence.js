93281e64eef4f378a63d57d8ef42799b2975465e
perezxalan <perezxalan@gmail.com>
2023-05-30 22:02:26 -0500

    day 3

--- Pointers/isSubSequence.js
+++ Pointers/isSubSequence.js
@@ -14,17 +14,18 @@ var isSubsequence = function(s, t) {
             return true;
         }
 
-    // pointer
+    // pointer to keep track of our progress through string s
     let j = 0;
 
-    // iterate through the array
+    // iterate through every character in string t
     for(let i = 0; i < t.length; i++) {
 
-        // check if it matches, move pointer
+        // check if current char in string t matches char in string s, move pointer
         if(s[j] == t[i]) {
             j++
       
         }
+        // if j is equal to string s length then that means we found every match 
         if(j === s.length) return true
     }
 
